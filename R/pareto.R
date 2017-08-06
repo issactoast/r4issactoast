7e3557eee8a08759d9dd2f407a1a45a5dd6cb7b3
Issac Lee <issactoast@gmail.com>
2017-08-06 14:52:06 -0500

    c function update

---
+++ R/pareto.R
@@ -0,0 +1,42 @@
+#' The probability density function of the pareto distribution.
+#'
+#' dpareto function evaluates the pdf of pareto dist. at given x with parameters.
+#'
+#' @param x the point where the pdf to be evaluated
+#' @param alpha the shape parameter of pareto dist.
+#' @param x_m the scale parameter of pareto dist.
+#' @param log.p use log.p = TRUE when you want
+#'  to get a result in a log scale
+#' @return the pdf value of the pareto dist.
+#' @examples
+#'   dpareto(1:5, 2, 3)
+#'   dpareto(1:5, 2, -3:3)
+#'   dpareto(1:5, 2:5, 3)
+#' @useDynLib r4issactoast paretodens
+#' @export
+dpareto <- function(x, alpha, x_m, log.p = FALSE){
+    if (!is.logical(log.p) || length(log.p) != 1){
+        stop("bad input for argument 'log'")
+    }
+
+    input_len <- c(length(x), length(alpha), length(x_m))
+    max_len <- max(input_len)
+
+    # Input vectorizing
+    density <- vector(mode="numeric", length = max_len)
+
+    # Call .C
+    result <- .C("paretodens",
+                 as.double(x),
+                 as.double(alpha),
+                 as.double(x_m),
+                 as.integer(input_len),
+                 as.integer(max_len),
+                 as.integer(log.p),
+                 value = as.double(density),
+                 PACKAGE = "r4issactoast")$value
+    if (any(is.nan(result))) {
+        warning("\n All parameters should be positive.\n NaN is generated.")
+    }
+    result
+}
---
+++ src/paretoRoutines.c
@@ -0,0 +1,55 @@
+#include <R.h>
+#include <R_ext/Rdynload.h>
+
+void paretodens(double *x, double *alpha, double *x_m,
+                 int *input_len, int *max_len, int *is_log,
+                 double *density){
+
+  // Intialization
+  int i, n, x_len, alpha_len, x_m_len, IsLog;
+
+  // Prepare recycling
+  n = *max_len;
+  IsLog = *is_log;
+  x_len = input_len[0];
+  alpha_len = input_len[1];
+  x_m_len = input_len[2];
+
+  // For loop in C
+  for (i = 0; i < n; i++){
+    density[i] = R_NegInf;
+  }
+
+  for (i = 0; i < n; i++){
+    if (alpha[i % alpha_len] <= 0.0 || x_m[i % x_m_len] <= 0.0) {
+      density[i] = R_NaN;
+    } else if(x[i % x_len] >= x_m[i % x_m_len]){
+      density[i] = log(alpha[i % alpha_len]) +
+        alpha[i % alpha_len] * log(x_m[i % x_m_len]) -
+        (alpha[i % alpha_len] + 1.0) * log(x[i % x_len]);
+    }
+  }
+
+  if (!IsLog) {
+    for ( i = 0; i < n; i++){
+      density[i] = exp( density[i] );
+    }
+  }
+}
+
+static const
+R_CMethodDef cMethods[] = {
+    {"paretodens", (DL_FUNC) &paretodens, 7},
+    NULL
+};
+
+void
+R_init_paretoRoutiones(DllInfo *info){
+    /* Register the .C routines.
+     No .Call, .Fortran(), .External()
+     routines, pass those arrays as NULL.
+     */
+    R_registerRoutines(info,
+                       cMethods, NULL, NULL, NULL);
+    R_useDynamicSymbols(info, FALSE);
+}
